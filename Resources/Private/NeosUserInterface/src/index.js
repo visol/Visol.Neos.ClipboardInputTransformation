af854315ea15c4ff8db11dc0f1f262c55029ef4f
mhsdesign <85400359+mhsdesign@users.noreply.github.com>
2023-03-21 08:48:57 +0100

    Initial Working On-Paste Replacer

---
+++ Resources/Private/NeosUserInterface/src/index.js
@@ -0,0 +1,72 @@
+import manifest from "@neos-project/neos-ui-extensibility";
+
+const addPlugin = (Plugin, isEnabled) => (ckEditorConfiguration, options) => {
+	if (!isEnabled || isEnabled(options.editorOptions, options)) {
+		return {
+			...ckEditorConfiguration,
+			plugins: [
+				...(ckEditorConfiguration.plugins ?? []),
+				Plugin
+			]
+		};
+	}
+	return ckEditorConfiguration;
+};
+
+const createInputSubstitutionPlugin = (replacementsConfiguration) => function (editor) {
+	// https://ckeditor.com/docs/ckeditor5/16.0.0/framework/guides/deep-dive/clipboard.html#input-pipeline
+
+	const replacements = replacementsConfiguration.map(({search, replace}) => [search, replace])
+
+	const replaceText = (textElement, newText) => {
+		// todo use `UpcastWriter.createText` and `UpcastWriter.replace` but we cant get ahold of the writer until: https://github.com/neos/neos-ui/issues/3436
+		textElement._textData = newText
+	}
+
+	const traverseTextElements = (element, textVisitor) => {
+		if (element.is("text")) {
+			textVisitor(element)
+			return
+		}
+
+		if (element.childCount) {
+			for (const child of element.getChildren()) {
+				traverseTextElements(child, textVisitor)
+			}
+		}
+	}
+
+	// fyi when ckeditor is updated, the api changes to `editor.plugins.get('ClipboardPipeline')`
+	editor.plugins.get('Clipboard').on(
+		'inputTransformation',
+		(evt, data) => {
+			traverseTextElements(data.content, (textElement) => {
+				/** @type string */
+				const textContents = textElement.data;
+
+				let newTextContents = textContents;
+
+				for (const [from, to] of replacements) {
+					newTextContents = newTextContents.replaceAll(from, to)
+				}
+
+				if (textContents !== newTextContents) {
+					replaceText(textElement, newTextContents)
+				}
+			})
+		},
+		{priority: 'high'}
+	);
+}
+
+manifest('Visol.Neos.InputSubstitution', {}, (globalRegistry, {frontendConfiguration}) => {
+	const ckEditorRegistry = globalRegistry.get('ckEditor5');
+	const replacementsConfiguration = frontendConfiguration['Visol.Neos.InputSubstitution'].replacements;
+
+	const config = ckEditorRegistry.get('config');
+
+	const InputSubstitutionPlugin = createInputSubstitutionPlugin(replacementsConfiguration);
+	config.set('Visol.Neos.InputSubstitution', addPlugin(InputSubstitutionPlugin, () => true));
+
+	return config;
+});
